ff807bccc3821beedd76e85d7219235c1ed2257f
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2023-07-18 11:56:44 +0100

    Clean up the M4 demo.

--- psoc6-cm4-hello-world/src/main.rs
+++ psoc6-cm4-hello-world/src/main.rs
@@ -7,19 +7,21 @@ use panic_semihosting as _;
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!");
+    hprintln!("Hello, I am the Cortex-M4!");
 
-    let p = unsafe { psoc6_pac::Peripherals::steal() };
+    let _p = unsafe { psoc6_pac::Peripherals::steal() };
     let cp = unsafe { cortex_m::Peripherals::steal() };
 
     // Can we read Cortex-M registers
+    // The Cortex-M0+ will report 0x41_0_cc60_1 (ARM, Rev 0, ARMv6-M Cortex-M0+, Patch 1).
+    // The Cortex-M4F will report 0x41_0_fc24_1 (ARM, Rev 0, Cortex-M4, Patch 1).
     hprintln!("CPUID.base = 0x{:08x}", cp.CPUID.base.read());
 
-    // Can we read PSoC6 registers?
-    hprintln!(
-        "CPUSS.cm4_clock_ctl = 0x{:08x}",
-        p.CPUSS.cm4_clock_ctl.read().bits()
-    );
-
-    panic!("End of main()");
+    // 50 MHz seems to be our default speed
+    let mut delay = cortex_m::delay::Delay::new(cp.SYST, 50_000_000);
+    loop {
+        // Set GPIO pin high
+        hprintln!("Hello!");
+        delay.delay_ms(1000);
+    }
 }
